a5c30f853705de7bbc5de22cd8df31c72090fc27
root <ivi3001@yahoo.de>
2020-03-19 10:41:18 +0000

    added ball_chaser launch file

--- src/ball_chaser/src/drive_bot.cpp
+++ src/ball_chaser/src/drive_bot.cpp
@@ -2,29 +2,48 @@
 #include "geometry_msgs/Twist.h"
 #include "ball_chaser/DriveToTarget.h"
 
+#include <sstream>
+
 ros::Publisher motor_command_publisher;
 
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
 ball_chaser::DriveToTarget::Response& res) {
 
-    ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f, (float)req.linear_x,     (float)req.angular_z);
+    ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f", 
+                (float)req.linear_x, (float)req.angular_z);
     
+    geometry_msgs::Twist motor_command;
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
+    motor_command_publisher.publish(motor_command);
+
+    std::ostringstream oss;
+    oss.precision(2);
+    oss << "Motor command published - linear_x:" << req.linear_x 
+        << ", angular_z:" << req.angular_z;
+    res.msg_feedback = oss.str();
+    
+    ROS_INFO_STREAM(res.msg_feedback);
+
     return true;
 }
 
 
 int main(int argc, char** argv) {
-    ros:Init(argc, argv, "drive_bot");
+
+    ros::init(argc, argv, "drive_bot");
 
     ros::NodeHandle n;
 
-    motor_command_publisher = n.advertice<geometry_msgs::Twist>("/cmd_vel", 10);
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
     //todo: drive service
+    ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
 
-    //todo: 
-    
+    ROS_INFO("Ready to send motor commands");
 
+   
+    ros::spin();
     
 
     return 0;
